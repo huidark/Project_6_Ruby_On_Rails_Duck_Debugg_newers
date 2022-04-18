8c98a0de09e403ed8ff41b22b774051bf976bada
dishon18 <dishon.mmanyi@aol.com>
2022-04-18 19:35:49 -0400

    update dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   # get '/dashboardU', to: 'users#index'
   get '/dashboardA', to: 'static_pages#dashboardA'
-  get '/dashboardU', to: 'static_pages#dashboardU'
+  get '/dashboardU', to: 'users#dashboardU'
   resources :groups
 
 
